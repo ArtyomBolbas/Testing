a6083b37a0ed79fa0601846f150c439561409583
Artyom Bolbas <artyombolbas@yandex.ru>
2018-10-01 10:27:38 +0300

    add v0.21 "Testing" package: "view" -(class) View.java

---
+++ src/view/View.java
@@ -0,0 +1,30 @@
+package view;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class View {
+
+	public void showWindowControllerStarting(Stage primaryStage) {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/view/Starting.fxml"));
+		try {
+			loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Parent root = loader.getRoot();
+		primaryStage.setScene(new Scene(root));
+		primaryStage.setResizable(true);
+		primaryStage.setMinHeight(700);
+		primaryStage.setMinWidth(800);
+		primaryStage.setTitle("Testing - authorization");
+		primaryStage.show();
+	}
+	
+	
+}
